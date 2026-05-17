2e0140bbef9004ab284a6f50ee8e05c1ff1a3ad8
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2026-05-16 20:43:34 -0700

    fix: Refactor TestOutcome enum with updated summaries

--- TrxLib/TestOutcome.cs
+++ TrxLib/TestOutcome.cs
@@ -2,36 +2,82 @@ namespace TrxLib;
 
 /// <summary>
 /// Represents the possible outcomes of a test execution.
+/// Checked against https://github.com/microsoft/vstest/blob/ba0077af37594366d76e0b8455143b982de3d416/src/Microsoft.TestPlatform.Extensions.TrxLogger/ObjectModel/TestOutcome.cs
 /// </summary>
 public enum TestOutcome
 {
     /// <summary>
-    /// The test was executed successfully and all assertions passed.
+    /// There was a system error while we were trying to execute a test.
     /// </summary>
-    Passed,
-    
+    Error,
+
     /// <summary>
-    /// The test was executed but failed one or more assertions or threw an exception.
+    /// Test was executed, but there were issues.
+    /// Issues may involve exceptions or failed assertions.
     /// </summary>
     Failed,
-    
+
     /// <summary>
-    /// The test was not executed, typically due to being skipped or ignored.
-    /// </summary>
-    NotExecuted,
-    
-    /// <summary>
-    /// The test execution completed but the result was inconclusive.
-    /// </summary>
-    Inconclusive,
-    
-    /// <summary>
-    /// The test execution exceeded the allowed time limit.
+    /// The test timed out
     /// </summary>
     Timeout,
-    
+
     /// <summary>
-    /// The test is awaiting execution or further action.
+    /// Test was aborted.
+    /// This was not caused by a user gesture, but rather by a framework decision.
+    /// </summary>
+    Aborted,
+
+    /// <summary>
+    /// Test has completed, but we can't say if it passed or failed.
+    /// May be used for aborted tests...
+    /// </summary>
+    Inconclusive,
+
+    /// <summary>
+    /// Test was executed w/o any issues, but run was aborted.
+    /// </summary>
+    PassedButRunAborted,
+
+    /// <summary>
+    /// Test had it chance for been executed but was not, as ITestElement.IsRunnable == false.
+    /// </summary>
+    NotRunnable,
+
+    /// <summary>
+    /// Test was not executed.
+    /// This was caused by a user gesture - e.g. user hit stop button.
+    /// </summary>
+    NotExecuted,
+
+    /// <summary>
+    /// Test run was disconnected before it finished running.
+    /// </summary>
+    Disconnected,
+
+    /// <summary>
+    /// To be used by Run level results.
+    /// This is not a failure.
+    /// </summary>
+    Warning,
+
+    /// <summary>
+    /// Test was executed w/o any issues.
+    /// </summary>
+    Passed,
+
+    /// <summary>
+    /// Test has completed, but there is no qualitative measure of completeness.
+    /// </summary>
+    Completed,
+
+    /// <summary>
+    /// Test is currently executing.
+    /// </summary>
+    InProgress,
+
+    /// <summary>
+    /// Test is in the execution queue, was not started yet.
     /// </summary>
     Pending
 }
